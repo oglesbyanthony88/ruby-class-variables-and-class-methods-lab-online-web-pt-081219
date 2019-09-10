4568041b1e7318cfc239e14b39a3d01792dfd747
Anthony Oglesby <ogle1ant@gmail.com>
2019-09-10 16:49:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,96 +1,42 @@
-class Song
-  attr_accessor :name, :artist, :genre
-  
-  @@count = 0
-  @@artist = []
-  @@genre = []
-  
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-    
-    @@count += 1
-    
-    @@genre << genre
-    @@artist << artist
-  end
-  
-  def self.count
-    @@count
-  end
-  
-  def self.artists
-    @@artists.uniq
-  end
-  
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
-      end
-    end
-    artist_count
-end
-
- def self.genres
-    @@genres.uniq
-  end
-
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1 
-      else
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
-  end
-end
-
-
 # class Song
 #   attr_accessor :name, :artist, :genre
-
+  
 #   @@count = 0
-#   @@artists = []
-#   @@genres = []
-
+#   @@artist = []
+#   @@genre = []
+  
 #   def initialize(name, artist, genre)
 #     @name = name
 #     @artist = artist
 #     @genre = genre
-#     @@count +=1
-#     @@genres << genre
-#     @@artists << artist
+    
+#     @@count += 1
+    
+#     @@genre << genre
+#     @@artist << artist
 #   end
-
+  
 #   def self.count
 #     @@count
 #   end
-
+  
 #   def self.artists
 #     @@artists.uniq
 #   end
-
+  
 #   def self.artist_count
 #     artist_count = {}
 #     @@artists.each do |artist|
 #       if artist_count[artist]
-#         artist_count[artist] += 1 
+#         artist_count[artist] += 1
 #       else
 #         artist_count[artist] = 1
 #       end
 #     end
 #     artist_count
-#   end
+# end
 
-#   def self.genres
+# def self.genres
 #     @@genres.uniq
 #   end
 
@@ -106,3 +52,57 @@ end
 #     genre_count
 #   end
 # end
+
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+end
